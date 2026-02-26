c05e7e69cb0465f1d8dd7f42160cf6f8ba09508d
austin <eliottroussille@gmail.com>
2026-02-26 13:04:08 +0100

    refactor Badge component to include color prop for dynamic styling

--- utils/Badge.svelte
+++ utils/Badge.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
-	/** @type {{text: string, className?: string}} */
-	let { text, className = '' } = $props();
+	/** @type {{text: string, color: string, className?: string}} */
+	let { text, color, className = '' } = $props();
+
+	const colorClasses: string = $derived(`border-${color}/40 bg-${color}/15 text-${color}`);
 </script>
 
 <span
-	class={`inline-flex rounded-full border px-2.5 py-1 text-[0.6rem] uppercase ${className}`.trim()}
+	class={`inline-flex rounded-full border px-2.5 py-1 text-[0.6rem] uppercase ${colorClasses} ${className}`.trim()}
 >
 	{text}
 </span>
